aff7639500dd07b60ece3fcb1bd4320d82b14af7
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2023-08-22 22:27:56 -0300

    teste2

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-import { job } from "./src/app.js";
+import { launch } from "puppeteer";
+import { config } from "dotenv";
 
-job.invoke();
+config();
+
+export async function robot() {
+  const browser = await launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(process.env.PAGE, {
+    waitUntil: "networkidle2",
+  });
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+  );
+
+  await page.locator("#user_name").fill(process.env.LOGIN);
+  await page.locator("#user_password").fill(process.env.PASS);
+  await page.locator("#sysverb_login").click();
+
+  await page.waitForNavigation({ waitUntil: "networkidle2" });
+
+  await browser.close();
+}
+
+(async () => {
+  robot();
+})();
--- src/app.js
+++
@@ -1,6 +0,0 @@
-import { scheduleJob } from "node-schedule";
-import { robot } from "./robot.js";
-
-export const job = scheduleJob("* */60 * * * *", async () => {
-  robot();
-});
